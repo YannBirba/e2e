687a03bb365eb03355dd613230d90a4da26edbda
Yann Birba <birbayann@gmail.com>
2022-12-23 11:49:41 +0100

    fix: use test id for e2e

--- test/ztransfer.test.ts
+++ test/ztransfer.test.ts
@@ -4,31 +4,31 @@ test("go to the contact page when user click on the contact link", async ({
   page,
 }) => {
   await page.goto("http://localhost:5173");
-  await page.getByRole("link", { name: "Contacts" }).click();
+  await page.getByTestId("contacts").click();
   await expect(page).toHaveURL("http://localhost:5173/contacts");
-});
-test("go to the profile page when user click on the profile link", async ({
-  page,
-}) => {
-  await page.goto("http://localhost:5173");
-  await page.getByRole("link", { name: "Profile" }).click();
-  await expect(page).toHaveURL("http://localhost:5173/profile");
 });
 test("go to the files page when user click on the files link", async ({
   page,
 }) => {
   await page.goto("http://localhost:5173");
-  await page.getByRole("link", { name: "Fichiers" }).click();
+  await page.getByTestId("files").click();
   await expect(page).toHaveURL("http://localhost:5173/files");
 });
+test("go to the upload page when user click on the upload link", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:5173");
+  await page.getByTestId("upload").click();
+  await expect(page).toHaveURL("http://localhost:5173/upload");
+});
 
-// test.skip("go to the home page when user click on the home link", async ({
-//   page,
-// }) => {
-//   await page.goto("http://localhost:5173");
-//   await page.getByRole("link", { name: "Fichiers" }).click();
-//   await page.getByRole("link", { name: "Accueil" }).click();
-//   await expect(page).toHaveURL(
-//     "http://localhost:5173/" || "http://localhost:5173"
-//   );
-// });
+test("go to the home page when user click on the home link", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:5173");
+  await page.getByTestId("contacts").click();
+  await page.getByTestId("home").click();
+  await expect(page).toHaveURL(
+    "http://localhost:5173/" || "http://localhost:5173"
+  );
+});
